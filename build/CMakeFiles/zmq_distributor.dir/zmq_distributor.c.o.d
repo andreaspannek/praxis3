43ef2b2e5821c5276757635272a67b0be2c10c10
andreaspannek <andreas.pannek@outlook.de>
2025-01-29 13:51:11 +0100

    Initial commit

---
+++ build/CMakeFiles/zmq_distributor.dir/zmq_distributor.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/zmq_distributor.dir/zmq_distributor.c.o: \
+ /mnt/c/Users/andre/Desktop/praxis3/zmq_distributor.c \
+ /usr/include/stdc-predef.h
---
+++ build/CMakeFiles/zmq_worker.dir/zmq_worker.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/zmq_worker.dir/zmq_worker.c.o: \
+ /mnt/c/Users/andre/Desktop/praxis3/zmq_worker.c \
+ /usr/include/stdc-predef.h
